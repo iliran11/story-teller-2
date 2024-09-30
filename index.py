52e13d039278cd23230f75f4d680d5073944dfb0
Liran Cohen <iliran11@gmail.com>
2024-09-30 08:32:50 +0300

    formatter

--- index.py
+++ index.py
@@ -1,20 +1,20 @@
 import os
 from dotenv import load_dotenv
-from crewai import Agent,Crew,Task,Process
+from crewai import Agent, Crew, Task, Process
 from tasks import Create_chapter
 
 
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_SECRET')
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_SECRET")
 
 suggestor = Agent(
     role="The ideas person",
     goal="Discover a unique and intereting topic for a story happened any time in the past. Involving war and tactics",
     backstory="""You are a curious person who wants to learn about the world,
                  from every time in the world. You wish to uncover unique subjcets""",
-    verbose=True
+    verbose=True,
 )
 
-writer=Agent(
+writer = Agent(
     role="The writer of the story",
     goal="""You will add another chapter to a story each time you are referred to.""",
     backstory="""Your writing should be entertining.
@@ -22,7 +22,7 @@ writer=Agent(
             The english should be dead-simple.
             Each one of your chapters are rather short. 4-5 sentences typically.
               """,
-    verbose=True
+    verbose=True,
 )
 
 suggest_topic = Task(
@@ -30,19 +30,19 @@ suggest_topic = Task(
     expected_output="""A one setence topic describing the event by name and time.
                         The event should highlight an exceptional strategic shifting in human history.
                         The output should be used as a header for the story. So must be very concise.""",
-    agent=suggestor
+    agent=suggestor,
 )
 
-task_1 = Create_chapter(1,writer,[])
-task_2 = Create_chapter(2,writer,[task_1])
-task_3 = Create_chapter(3,writer,[task_1,task_2])
-task_4 = Create_chapter(    4,writer,[task_1,task_2,task_3])
+task_1 = Create_chapter(1, writer, [])
+task_2 = Create_chapter(2, writer, [task_1])
+task_3 = Create_chapter(3, writer, [task_1, task_2])
+task_4 = Create_chapter(4, writer, [task_1, task_2, task_3])
 
 crew = Crew(
-    agents=[suggestor,writer],
-    tasks=[suggest_topic, task_1,task_2,task_3,task_4],
+    agents=[suggestor, writer],
+    tasks=[suggest_topic, task_1, task_2, task_3, task_4],
     verbose=True,
-    process = Process.sequential
+    process=Process.sequential,
 )
 
 result = crew.kickoff()
